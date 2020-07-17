131e2d30ef063cd1f297c8f83afddceef1bddfa4
Bread <withbbang@gmail.com>
2020-07-17 14:16:06 +0900

    commit

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -8,6 +8,21 @@ export default {
     likes: ({ id }) => prisma.user({ id }).likes(),
     comments: ({ id }) => prisma.user({ id }).comments(),
     rooms: ({ id }) => prisma.user({ id }).rooms(),
+    postsCount: ({ id }) =>
+      prisma
+        .postsConnection({ where: { user: { id } } })
+        .aggregate()
+        .count(),
+    followingCount: ({ id }) =>
+      prisma
+        .usersConnection({ where: { followers_some: { id } } })
+        .aggregate()
+        .count(),
+    followersCount: ({ id }) =>
+      prisma
+        .usersConnection({ where: { following_some: { id } } })
+        .aggregate()
+        .count(),
     fullName: (parent) => {
       return `${parent.firstName} ${parent.lastName}`;
     },
